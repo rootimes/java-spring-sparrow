b5a04fd99b6763441373de90389ce9b59105864d
rootimes <rootimes8596@gmail.com>
2026-03-04 19:10:06 +0800

    feat: add logging aspect and enable AOP in project configuration

--- legacy/src/main/java/sparrow/ProjectConfig.java
+++ legacy/src/main/java/sparrow/ProjectConfig.java
@@ -1,9 +1,13 @@
 package sparrow;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@ComponentScan(basePackages = "sparrow")
+@EnableAspectJAutoProxy
 public class ProjectConfig {
     @Bean
     public MessageService messageService() {
