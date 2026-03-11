4dc134ae54565cffe76f533cf41ad3e04cc3fe12
rootimes <rootimes8596@gmail.com>
2026-03-11 17:57:07 +0800

    feat: add User and UserRepository classes for user management

---
+++ legacy/src/main/java/sparrow/user/User.java
@@ -0,0 +1,95 @@
+package sparrow.user;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import sparrow.post.entity.Post;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(length = 500)
+    private String description;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private Set<Post> posts = new HashSet<>();
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Post> getPosts() {
+        return posts;
+    }
+
+    public void addPost(Post post) {
+        if (post != null) {
+            this.posts.add(post);
+            post.setUser(this);
+        }
+    }
+
+    public void removePost(Post post) {
+        if (post != null) {
+            this.posts.remove(post);
+            post.setUser(null);
+        }
+    }
+}
